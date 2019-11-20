bfe5761fcecd14cf38d6bb53dc758aa30459df19
Petr Cermak <cermak.petr6@gmail.com>
2019-11-20 19:05:51 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,95 @@
+### Firmy.cz Scraper
+
+Firmy.cz Scraper is an [Apify actor](https://apify.com/actors) for extracting data about companies from [Firmy.cz](https://www.firmy.cz). It allows you to search for any category of companies and extract detailed information about each one. It is build on top of [Apify SDK](https://sdk.apify.com/) and you can run it both on [Apify platform](https://my.apify.com) and locally.
+
+- [Input](#input)
+- [Output](#output)
+- [Compute units consumption](#compute-units-consumption)
+- [Extend output function](#extend-output-function)
+
+### Input
+
+| Field | Type | Description | Default value
+| ----- | ---- | ----------- | -------------|
+| startUrls | array | List of [Request](https://sdk.apify.com/docs/api/request#docsNav) objects that will be deeply crawled. The URL can be top level like `https://www.firmy.cz`, any category URL or company detail URL | `[{ "url": "https://www.firmy.cz" }]`|
+| maxItems | number | Maximum number of actor pages that will be scraped | all found |
+| extendOutputFunction | string | Function that takes a JQuery handle ($) as argument and returns data that will be merged with the default output. More information in [Extend output function](#extend-output-function) | |
+| proxyConfiguration | object | Proxy settings of the run. If you have access to Apify proxy, leave the default settings. If not, you can set `{ "useApifyProxy": false" }` to disable proxy usage | `{ "useApifyProxy": true }`|
+
+### Output
+
+Output is stored in a dataset. Each item is an information about an actor. Example:
+
+```
+{
+    "title": "ClaimCloud.cz",
+    "address": "Revoluční 1403/28, 11000 Praha, Nové Město",
+    "latitude": 50.092247009277344,
+    "longitude": 14.427606582641602,
+    "description": "Firma byla založena za účelem pomáhat cestujícím v letecké dopravě v získávání finančních kompenzací u leteckých společností, a to v oblastech zpoždění letů, zrušení letů, zmeškání návazných letů, odepření nástupu na palubu, snížení přepravní třídy nebo v případě problémů se zavazadly.",
+    "categories": [
+        "Cestovní služby"
+    ],
+    "rating": null,
+    "ratingcount": 0,
+    "phone": "+420 777 993 788",
+    "emails": [
+        "info@claimcloud.cz",
+        "support@claimcloud.cz"
+    ],
+    "website": "claimcloud.cz",
+    "url": "https://www.firmy.cz/detail/12998843-claimcloud-cz-praha-nove-mesto.html"
+}
+```
+
+### Compute units consumption
+Keep in mind that it is much more efficient to run one longer scrape (at least one minute) than more shorter ones because of the startup time.
+
+The average consumption is **1 Compute unit for 1000 actor pages** scraped
+
+### Extend output function
+
+You can use this function to update the default output of this actor. This function gets a JQuery handle `$` as an argument so you can choose what data from the page you want to scrape. The output from this will function will get merged with the default output.
+
+The return value of this function has to be an object!
+
+You can return fields to achive 3 different things:
+- Add a new field - Return object with a field that is not in the default output
+- Change a field - Return an existing field with a new value
+- Remove a field - Return an existing field with a value `undefined`
+
+
+```
+($) => {
+    return {
+        image: $('.photoMain').attr('src'),
+        title: 'the-new-title',
+        rating: undefined,
+    }
+}
+```
+This example will add a new field `image`, change the `title` field and remove the `rating` field
+```
+{
+    "title": "the-new-title",
+    "address": "Revoluční 1403/28, 11000 Praha, Nové Město",
+    "latitude": 50.092247009277344,
+    "longitude": 14.427606582641602,
+    "description": "Firma byla založena za účelem pomáhat cestujícím v letecké dopravě v získávání finančních kompenzací u leteckých společností, a to v oblastech zpoždění letů, zrušení letů, zmeškání návazných letů, odepření nástupu na palubu, snížení přepravní třídy nebo v případě problémů se zavazadly.",
+    "categories": [
+        "Cestovní služby"
+    ],
+    "ratingcount": 0,
+    "phone": "+420 777 993 788",
+    "emails": [
+        "info@claimcloud.cz",
+        "support@claimcloud.cz"
+    ],
+    "website": "claimcloud.cz",
+    "url": "https://www.firmy.cz/detail/12998843-claimcloud-cz-praha-nove-mesto.html",
+    "image": "https://d48-a.sdn.cz/d_48/c_img_E_G/wBmPnX.png?fl=res,600,400,3,ffffff"
+}
+```
+
+### Epilogue
+Thank you for trying my actor. I will be very glad for a feedback that you can send to my email `petr.cermak@apify.com`. If you find any bug, please create an issue on the [Github page](https://github.com/metalwarrior665/apify-store-scraper).
---
+++ main.js
@@ -0,0 +1,145 @@
+// This is the main Node.js source code file of your actor.
+// It is referenced from the "scripts" section of the package.json file,
+// so that it can be started by running "npm start".
+
+// Include Apify SDK. For more information, see https://sdk.apify.com/
+const Apify = require('apify');
+const log = Apify.log;
+
+const isObject = (val) => typeof val === 'object' && val !== null && !Array.isArray(val);
+
+const extractData = () => {
+    const $ = selector => document.querySelector(selector);
+    const $$ = selector => Array.from(document.querySelectorAll(selector));
+    const title = $('[itemprop="name"]').textContent.trim();
+    const address = $('[itemprop="address"]').textContent.trim();
+    const lat = $('[itemprop="latitude"]').getAttribute('content');
+    const lon = $('[itemprop="longitude"]').getAttribute('content');
+    const desc = $('[itemprop="description"]').textContent.trim();
+    const categories = $$('.category').map(c => c.textContent.trim());
+    const rating = $('[itemprop="ratingValue"]').getAttribute('content');
+    const rCount = $('[itemprop="ratingCount"]').textContent.trim();
+    const ratingCount = rCount ? parseInt(rCount) : 0;
+    const phone = $('[itemprop="telephone"]').textContent.trim();
+    const emails = $$('.companyMail').map(e => e.textContent.trim());
+    const websites = $$('.companyUrl').map(e => e.textContent.trim());
+    const result = {
+        "title": title,
+        "address": address.replace(/[\n\t]+/g, ''),
+        "latitude": lat ? parseFloat(lat) : null,
+        "longitude": lon ? parseFloat(lon) : null,
+        "description": desc,
+        "categories": categories,
+        "rating": (ratingCount && rating) ? parseInt(rating)/20 : null,
+        "ratingcount": ratingCount,
+        "phone": phone,
+        "emails": emails,
+        "website": websites[0] || null
+    };
+    return result;
+};
+
+Apify.main(async () => {
+    // Get input of the actor.
+    const input = await Apify.getInput();
+
+    // Open default dataset
+    const dataset = await Apify.openDataset();
+    let itemCount = (await dataset.getInfo()).cleanItemCount;
+
+    let extendOutputFunction;
+    try {
+        extendOutputFunction = eval(input.extendOutputFunction);
+    } catch (e) {
+        throw new Error(`extendOutputFunction is not a valid JavaScript! Error: ${e}`)
+    }
+    if (typeof extendOutputFunction !== "function") {
+        throw new Error(`extendOutputFunction is not a function! Please fix it or use just default output!`)
+    }
+
+    // Open a request queue and request list
+    let requestList = null;
+    const requestQueue = await Apify.openRequestQueue();
+    if(input.startURLs){
+        requestList = new Apify.RequestList({ sources: input.startURLs });
+        await requestList.initialize();  
+    }
+    else{await requestQueue.addRequest({ url: 'https://www.firmy.cz/' });}
+
+    // Define a pattern of URLs that the crawler should visit
+    const pageSelector = 'a.imgLink, a.companyTitle, a.btnExtendPage';
+    const pseudoUrls = [new Apify.PseudoUrl('https://www.firmy.cz/[.+]')];
+
+    // Create a crawler that will use headless Chrome / Puppeteer to extract data
+    // from pages and recursively add links to newly-found pages
+    const crawler = new Apify.PuppeteerCrawler({
+        requestList,
+        requestQueue,
+
+        launchPuppeteerOptions: input.proxyConfiguration || {
+            useApifyProxy: true
+        },
+
+        // This function is called for every page the crawler visits
+        handlePageFunction: async ({ request, page }) => {
+            if(request.url.includes('/detail/')){                
+                try{await page.waitFor('[itemprop="ratingCount"]');}
+                catch(e){log.info('No rating count found.');}
+                await Apify.utils.puppeteer.injectJQuery(page);
+                const myResult = await page.evaluate(extractData);
+                myResult.url = request.url;
+
+                let userResult = {};
+                try {
+                    await page.evaluate(`window.eoFn = ${input.extendOutputFunction};`);
+                    userResult = await page.evaluate(async () => 
+                        JSON.stringify(await eoFn($), (k, v) => v === undefined ? 'to_be_deleted' : v)
+                    );
+                    userResult = JSON.parse(userResult);
+                    Object.keys(userResult).map(function(key, index) {
+                        if(userResult[key] === 'to_be_deleted'){
+                            userResult[key] = undefined;
+                        }
+                    });
+                } catch (e) {
+                    console.log(`extendOutputFunction crashed! Pushing default output. Please fix your function if you want to update the output. Error: ${e}`);
+                }
+
+                if (!isObject(userResult)) {
+                    console.log('extendOutputFunction has to return an object!');
+                    process.exit(1);
+                }
+                const result = Object.assign(myResult, userResult);
+
+                await dataset.pushData(result);
+                if(input.maxItems && ++itemCount >= input.maxItems){
+                    console.log('Maximum item count reached, finishing...');
+                    process.exit(0);
+                }
+            }
+            else{
+                try{await page.waitFor(pageSelector);}
+                catch(e){log.info('No sub-pages found.');}
+                await Apify.utils.enqueueLinks({ 
+                    page,
+                    selector: pageSelector,
+                    pseudoUrls,
+                    requestQueue
+                });
+            }
+        },
+
+        // This function is called for every page the crawler failed to load
+        // or for which the handlePageFunction() throws at least "maxRequestRetries"-times
+        handleFailedRequestFunction: async ({ request }) => {
+            console.log(`Request ${request.url} failed too many times`);
+            await Apify.pushData({
+                '#debug': Apify.utils.createRequestDebugInfo(request),
+            });
+        },
+
+        maxRequestRetries: 3
+    });
+
+    await crawler.run();
+});
